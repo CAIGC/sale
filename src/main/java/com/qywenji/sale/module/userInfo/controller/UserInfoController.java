e68c70feeba23a8b0c3909ca25fe2f338c4e6baf
caigc <550179687@qq.com>
2016-12-02 18:24:45 +0800

    12-2

--- src/main/java/com/qywenji/sale/module/userInfo/controller/UserInfoController.java
+++ src/main/java/com/qywenji/sale/module/userInfo/controller/UserInfoController.java
@@ -1,11 +1,48 @@
 package com.qywenji.sale.module.userInfo.controller;
 
+import com.alibaba.fastjson.JSONObject;
 import com.qywenji.sale.commons.controller.BaseController;
+import com.qywenji.sale.module.userInfo.bean.UserInfo;
+import com.qywenji.sale.module.userInfo.service.UserInfoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by CAI_GC on 2016/11/30.
  */
 @Controller
+@RequestMapping(value = "/userInfo")
 public class UserInfoController extends BaseController {
+
+    @Autowired
+    private UserInfoService userInfoService;
+
+
+    /**
+     * 微信关注或取消关注
+     * @param userInfoJson
+     * @return
+     */
+    @RequestMapping(value = "/wechat/handle")
+    @ResponseBody
+    public Object wxHandle(String userInfoJson){
+        UserInfo userInfo = JSONObject.parseObject(userInfoJson,UserInfo.class);
+        if(userInfo == null){
+            return super.error("");
+        }
+        UserInfo user = userInfoService.geByOpenId(userInfo.getOpenId());
+        if(user != null){
+            user.setHeadImgUrl(userInfo.getHeadImgUrl());
+            user.setNickname(userInfo.getNickname());
+            user.setProvince(userInfo.getProvince());
+            user.setCity(userInfo.getCity());
+            user.setSex(userInfo.getSex());
+            userInfoService.save(user);
+        }else{
+            userInfoService.save(userInfo);
+        }
+        return super.success();
+    }
 }
